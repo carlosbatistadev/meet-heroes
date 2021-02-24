c3f627501ebbad050f8d9a4f5d24d4cfdfe2318c
Carlos Alexandre <carloscoffeepy@gmail.com>
2021-02-24 13:18:14 -0300

    Home route configuration

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:meet_heroes/app/pages/home/home_bindings.dart';
+import 'package:meet_heroes/app/pages/home/home_page.dart';
+
+import 'app_routes.dart';
 
 GetMaterialApp appWidget() {
   return GetMaterialApp(
@@ -7,6 +11,23 @@ GetMaterialApp appWidget() {
     title: 'Conhecer Heróis',
     theme: ThemeData(
       primaryColor: Colors.orange[900],
+      appBarTheme: AppBarTheme(
+        brightness: Brightness.dark,
+        color: Colors.grey[50],
+        iconTheme: IconThemeData(
+          color: Colors.black87,
+        ),
+        actionsIconTheme: IconThemeData(
+          color: Colors.black87,
+        ),
+      ),
     ),
+    getPages: <GetPage>[
+      GetPage(
+        name: AppRoutes.HOME_ROUTE,
+        page: () => HomePage(Get.find()),
+        binding: HomeBindings(),
+      ),
+    ],
   );
 }
