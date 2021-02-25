a3bda033e51538cd32d4aa49c66d22696eb6e91c
Carlos Alexandre <carloscoffeepy@gmail.com>
2021-02-25 15:36:39 -0300

    Arrumando URL

--- lib/app/app_bindings.dart
+++ lib/app/app_bindings.dart
@@ -5,7 +5,7 @@ class AppBindings implements Bindings {
   void dependencies() {
     Get.lazyPut(() {
       final GetConnect _getConnect = GetConnect();
-      _getConnect.baseUrl = 'https://superheroapi.com/api/2114412465359745';
+      _getConnect.baseUrl = 'https://superheroapi.com/api/2114412465359745/';
       return _getConnect;
     });
   }
